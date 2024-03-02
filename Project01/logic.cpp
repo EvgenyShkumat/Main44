f13859eb57a0e9e9baa3c870b276de98c917bac2
Евгений Шкумат <kotbmasle@gmail.com>
2024-03-02 11:07:14 +0300

    task02 done

--- Project01/logic.cpp
+++ Project01/logic.cpp
@@ -24,6 +24,58 @@ double count_nonzero(int* vector, int length) {
 	return count;
 }
 
-double arithmetic_avg_nonzero(int sum, double count) {
+double arithmetic_avg(int sum, double count) {
 	return sum / count;
+}
+
+int max_element(int* vector, int length){
+	int max = *vector;
+
+	for (int i = 1; i < length; i++)
+	{
+		if (*(vector + i) > max) {
+			max = *(vector + i);
+		}
+	}
+
+	return max;
+}
+
+int min_element(int* vector, int length) {
+	int min = *vector;
+
+	for (int i = 1; i < length; i++)
+	{
+		if (*(vector + i) < min) {
+			min = *(vector + i);
+		}
+	}
+
+	return min;
+}
+
+int sum_without_min_max(int* vector, int length, int min, int max) {
+	int sum = 0;
+
+	for (int i = 0; i < length; i++)
+	{
+		if (*(vector + i) != min && *(vector + i) != max) {
+			sum += *(vector + i);
+		}
+	}
+
+	return sum;
+}
+
+double count_without_min_max(int* vector, int length, int min, int max) {
+	double count = 0;
+
+	for (int i = 0; i < length; i++)
+	{
+		if (*(vector + i) != min && *(vector + i) != max) {
+			count++;
+		}
+	}
+
+	return count;
 }
--- Project01/main.cpp
+++ Project01/main.cpp
@@ -6,7 +6,12 @@ string output(int* vector, int size);
 
 int sum_nonzero(int* vector, int length);
 double count_nonzero(int* vector, int length);
-double arithmetic_avg_nonzero(int sum, double count);
+double arithmetic_avg(int sum, double count);
+
+int min_element(int* vector, int length);
+int max_element(int* vector, int length);
+int sum_without_min_max(int* vector, int length, int min, int max);
+double count_without_min_max(int* vector, int length, int min, int max);
 
 int main() {
 	int size = 10;
@@ -14,12 +19,14 @@ int main() {
 
 	random_init(vector, size, 0, 9);
 
-	int sum = sum_nonzero(vector, size);
-	double count = count_nonzero(vector, size);
+	int min = min_element(vector, size);
+	int max = max_element(vector, size);
+
+	int sum = sum_without_min_max(vector, size, min, max);
+	int count = count_without_min_max(vector, size, min, max);
 
 	cout << "Vector:\n" << output(vector, size) << endl;
-	cout << "Avg arithmetic of non-zero elements: " <<
-		arithmetic_avg_nonzero(sum, count);
+	cout << "Avg arithmetic of elements without min and max: " << arithmetic_avg(sum, count);
 
 	return 0;
 }
